3ad64707ea2f5fcfd34cc8a8d582cdeaf1465ff3
Kovid Goyal <kovid@kovidgoyal.net>
2017-06-07 15:01:45 +0530

    version 0.3

--- setup.py
+++ setup.py
@@ -9,23 +9,25 @@ if not hasattr(sys, 'getwindowsversion'):
     cflags.append('-fvisibility=hidden')
 
 sources = glob.glob('src/*.c') + glob.glob('src/duktape/*.c')
+version = '0.3'
 
-setup(
-    name='dukpy',
-    version='0.2',
-    author='Kovid Goyal',
-    author_email='kovid@kovidgoyal.net',
-    description='JavaScript runtime environment for Python',
-    url='https://github.com/kovidgoyal/dukpy',
-    ext_modules=[
-        Extension('dukpy', sources=sources, extra_compile_args=cflags),
-    ],
-    test_suite='tests',
-    classifiers=[
-        'Development Status :: 4 - Beta',
-        'License :: OSI Approved :: MIT License',
-        'Programming Language :: Python :: 3.4',
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: JavaScript',
-    ], )
+if __name__ == '__main__':
+    setup(
+        name='dukpy',
+        version=version,
+        author='Kovid Goyal',
+        author_email='some@one.com',
+        description='JavaScript runtime environment for Python',
+        url='https://github.com/kovidgoyal/dukpy',
+        ext_modules=[
+            Extension('dukpy', sources=sources, extra_compile_args=cflags),
+        ],
+        test_suite='tests',
+        classifiers=[
+            'Development Status :: 4 - Beta',
+            'License :: OSI Approved :: MIT License',
+            'Programming Language :: Python :: 3.4',
+            'Programming Language :: Python :: 3.5',
+            'Programming Language :: Python :: 2.7',
+            'Programming Language :: JavaScript',
+        ], )
